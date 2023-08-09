71c4e8efc302038b22e8b3626066e102b46e415e
Abrahman-ra <126220430+abrahman-ra@users.noreply.github.com>
2023-08-09 15:24:43 +0300

    20th problem - edited

--- 20-longest-consecutive-sequence-medium/longestConsecutive.js
+++ 20-longest-consecutive-sequence-medium/longestConsecutive.js
@@ -1,15 +1,20 @@
 "use strict";
 function longestConsecutive(nums) {
-  if (nums.length <= 1) return nums.length;
-  let ref = {};
-  for (let i = 0; i < nums.length; i++) ref[nums[i]] = i;
-  //   let count = Object.keys(ref).length;
-  let count = 1;
-  for (let key in ref) {
-    if (ref[+key - 1] !== undefined) count++;
-  }
-  //   count++;
-  return count;
+    let count = 0;
+    let current = 0;
+    let numsSet = new Set(nums);
+    for (let num of numsSet) {
+        if (numsSet.has(+num - 1))
+            continue;
+        else {
+            current = 1;
+            while (numsSet.has(+num + 1)) {
+                current++;
+                count = current >= count ? current : count;
+                num++;
+            }
+            count = current >= count ? current : count;
+        }
+    }
+    return count;
 }
-console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); // 4
-console.log(longestConsecutive([9, 1, 4, 7, 3, 0, 5, 8, -1, 6])); // 7
--- 20-longest-consecutive-sequence-medium/longestConsecutive.ts
+++ 20-longest-consecutive-sequence-medium/longestConsecutive.ts
@@ -1,14 +1,19 @@
 function longestConsecutive(nums: number[]): number {
-  if (nums.length <= 1) return nums.length;
-  let ref: { [value: number]: number } = {};
-  for (let i = 0; i < nums.length; i++) ref[nums[i]] = i;
-  let count = Object.keys(ref).length;
-  for (let key in ref) {
-    if (ref[+key - 1] === undefined) count--;
+  let count = 0;
+  let current = 0;
+  let numsSet = new Set(nums);
+
+  for (let num of numsSet) {
+    if (numsSet.has(+num - 1)) continue;
+    else {
+      current = 1;
+      while (numsSet.has(+num + 1)) {
+        current++;
+        count = current >= count ? current : count;
+        num++;
+      }
+      count = current >= count ? current : count;
+    }
   }
-  count++;
   return count;
 }
-
-console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); // 4
-console.log(longestConsecutive([9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6])); // 7
