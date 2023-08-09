89ccd77db3b9b1139a7149771fcb6ef1e697b71f
Abrahman-ra <126220430+abrahman-ra@users.noreply.github.com>
2023-08-09 16:27:34 +0300

    21st problem

---
+++ 21-best-time-to-but-and-sell-stock-easy/maxProfit.js
@@ -0,0 +1,14 @@
+"use strict";
+function maxProfit(prices) {
+  let max = 0;
+  let current = 0;
+  let iMin = 0;
+  let i = 0;
+  while (i < prices.length) {
+    current = prices[i] - prices[iMin];
+    if (prices[i] < prices[iMin]) iMin = i;
+    max = Math.max(current, max);
+    i++;
+  }
+  return max;
+}
---
+++ 21-best-time-to-but-and-sell-stock-easy/maxProfit.ts
@@ -0,0 +1,15 @@
+function maxProfit(prices: number[]): number {
+  let max = 0;
+  let current = 0;
+  let iMin = 0;
+  let i = 0;
+  while (i < prices.length) {
+    current = prices[i] - prices[iMin];
+    if (prices[i] < prices[iMin]) iMin = i;
+    max = Math.max(current, max);
+    i++;
+  }
+  return max;
+}
+
+console.log(maxProfit([2, 4, 1]));
