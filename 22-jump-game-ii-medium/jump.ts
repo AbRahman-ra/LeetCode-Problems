6aecdf1fba84c78fa87b09bf0f62ce7624d47dd1
Abrahman-ra <126220430+abrahman-ra@users.noreply.github.com>
2023-08-11 18:15:37 +0300

    22nd problem

---
+++ 22-jump-game-ii-medium/jump.ts
@@ -0,0 +1,25 @@
+function jump(nums: number[]): number {
+  if (nums.length <= 2) return nums.length - 1;
+  let count = 1;
+  let i = 0;
+  let m = nums[0];
+  let iMax = 0;
+  let max = nums[0];
+  while (m < nums.length - 1 && i < nums.length) {
+    if (i <= m) {
+      iMax = i + nums[i] > iMax + max ? i : iMax;
+      max = nums[iMax];
+      i++;
+    } else {
+      m = iMax + max;
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(jump([2, 3, 1, 1, 4])); // 2
+console.log(jump([2, 3, 0, 1, 4])); // 2
+console.log(jump([7, 0, 9, 6, 9, 6, 1, 7, 9, 0, 1, 2, 9, 0, 3])); // 2
+console.log(jump([1, 1, 1, 1])); // 2
+console.log(jump([1, 1, 2, 1, 1])); // 3
