f2ba870b7f70bccea9635370d38f7549cd109ef4
Abrahman-ra <126220430+abrahman-ra@users.noreply.github.com>
2023-08-12 08:16:05 +0300

    23rd problem

---
+++ 23-search-insert-position-easy/searchInsert.ts
@@ -0,0 +1,17 @@
+function searchInsert(nums: number[], target: number): number {
+  let sta = 0; // start
+  let end = nums.length - 1; // end
+  let mid = (sta + end) / 2 - ((sta + end) % 2) / 2; // middle
+
+  while (sta < end) {
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] > target) {
+      end = mid - 1;
+      mid = (sta + end) / 2 - ((sta + end) % 2) / 2;
+    } else {
+      sta = mid + 1;
+      mid = (sta + end) / 2 - ((sta + end) % 2) / 2;
+    }
+  }
+  return nums[mid] >= target ? mid : ++mid;
+}
