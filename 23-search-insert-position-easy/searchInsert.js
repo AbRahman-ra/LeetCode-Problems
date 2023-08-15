47bc5887e4551a89f5b8bea568ad77e896ba1a08
Abrahman-ra <126220430+abrahman-ra@users.noreply.github.com>
2023-08-15 08:22:09 +0300

    24th problem

--- 23-search-insert-position-easy/searchInsert.js
+++ 23-search-insert-position-easy/searchInsert.js
@@ -1,17 +1,19 @@
 "use strict";
 function searchInsert(nums, target) {
-  let sta = 0; // start
-  let end = nums.length - 1; // end
-  let mid = (sta + end) / 2 - ((sta + end) % 2) / 2; // middle
-  while (sta < end) {
-    if (nums[mid] === target) return mid;
-    else if (nums[mid] > target) {
-      end = mid - 1;
-      mid = (sta + end) / 2 - ((sta + end) % 2) / 2;
-    } else {
-      sta = mid + 1;
-      mid = (sta + end) / 2 - ((sta + end) % 2) / 2;
+    let sta = 0; // start
+    let end = nums.length - 1; // end
+    let mid = (sta + end) / 2 - ((sta + end) % 2) / 2; // middle
+    while (sta < end) {
+        if (nums[mid] === target)
+            return mid;
+        else if (nums[mid] > target) {
+            end = mid - 1;
+            mid = (sta + end) / 2 - ((sta + end) % 2) / 2;
+        }
+        else {
+            sta = mid + 1;
+            mid = (sta + end) / 2 - ((sta + end) % 2) / 2;
+        }
     }
-  }
-  return nums[mid] >= target ? mid : ++mid;
+    return nums[mid] >= target ? mid : ++mid;
 }
---
+++ 24-best-time-to-buy-and-sell-stock-ii-medium/maxProfit.ts
@@ -0,0 +1,20 @@
+function maxProfit(prices: number[]): number {
+  let max = 0;
+  let s = 0; // Selling Pointer
+  let b = 0; // Buying Pointer
+
+  while (s < prices.length) {
+    if (prices[s] <= prices[s - 1]) {
+      max += prices[s - 1] - prices[b];
+      b = s;
+    } else if (s === prices.length - 1) max += prices[s] - prices[b];
+    s++;
+  }
+
+  return max;
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 7
+console.log(maxProfit([1, 2, 3, 4, 5])); // 4
+console.log(maxProfit([7, 6, 4, 3, 1])); // 0
+console.log(maxProfit([1, 3, 2])); // 2
