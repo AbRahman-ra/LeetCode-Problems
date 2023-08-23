091543778cd5bd031fbd8aff9a2a363006b82399
Abrahman-ra <126220430+abrahman-ra@users.noreply.github.com>
2023-08-23 10:23:57 +0300

    minor fix

--- 24-best-time-to-buy-and-sell-stock-ii-medium/maxProfit.ts
+++ 24-best-time-to-buy-and-sell-stock-ii-medium/maxProfit.ts
@@ -18,3 +18,5 @@ console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 7
 console.log(maxProfit([1, 2, 3, 4, 5])); // 4
 console.log(maxProfit([7, 6, 4, 3, 1])); // 0
 console.log(maxProfit([1, 3, 2])); // 2
+
+export {};
